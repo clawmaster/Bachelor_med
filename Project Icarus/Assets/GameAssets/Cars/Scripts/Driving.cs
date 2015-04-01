e2a75e18ddc9c59d1355996efbbc627dfacc7af7
clawmaster <claes9@yahoo.dk>
2015-04-01 14:47:35 +0200

    Improved cars, now with acceleration
    Speeds up if far enough from destination, and slows down the closer it
    gets.

--- Project Icarus/Assets/GameAssets/Cars/Scripts/Driving.cs
+++ Project Icarus/Assets/GameAssets/Cars/Scripts/Driving.cs
@@ -4,16 +4,19 @@ using System.Collections;
 public class Driving : MonoBehaviour {
 
 	public Transform mover; //the object being moved
+	private int speed = 0;
 	public float SnapTo = 0.5f; //how close we get before snapping to the desination
 	private Vector3 destination = Vector3.zero; //where we want to move
 
 	// possible movement patterns.
+	private int groupCars;
 	private Vector3[] NextCheckpoint = new Vector3[3];
 	private int currentCheckPoint = 0;
 
 
 	// Use this for initialization
 	void Start () {
+
 		mover = this.transform;
 		destination = mover.position; //set the destination to the objects position when the script is run the first time
 		NextCheckpoint[0] = new Vector3(-105,0,-105); NextCheckpoint[1] = new Vector3(-105,0,0); NextCheckpoint[2] = new Vector3(0,0,0);
@@ -26,14 +29,28 @@ public class Driving : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-		//makes character look at mouse
+		//accelerate the car.
+		Debug.Log (destination.x + destination.z);
+		if (Mathf.Abs(mover.position.x + mover.position.z) - Mathf.Abs(destination.x + destination.z) > 25 && speed < 30) 
+		{
+			speed = speed + 1;
+		} 
+		else if (speed > 5) 
+		{
+			speed = speed - 1;
+		}
+			//makes character look at mouse
 		Vector3 newRotation = Quaternion.LookRotation(destination - mover.position).eulerAngles;
 		newRotation.x = 0;
 		newRotation.z = 0;
 		newRotation.y = newRotation.y - 180;
 		Quaternion QnewRotation = Quaternion.Euler(newRotation);
-		mover.rotation = Quaternion.Lerp(mover.rotation,QnewRotation,Time.deltaTime*3);
+		//Bringing car to stop if need to turn. 
+		if (mover.rotation != QnewRotation) 
+		{
+			speed = 5;
+		}
+		mover.rotation = Quaternion.Lerp(mover.rotation,QnewRotation,Time.deltaTime * speed);
 
 		//check for giving a new checkpoint.
 		if (mover.position == destination)
@@ -52,7 +69,7 @@ public class Driving : MonoBehaviour {
 			mover.position = destination; //snap to destination
 		}else
 		{
-			mover.position = Vector3.MoveTowards (mover.transform.position, destination, Time.deltaTime * 5); //move toward destination
+			mover.position = Vector3.MoveTowards (mover.transform.position, destination, Time.deltaTime * speed); //move toward destination
 		}
 	
 	}
--- Project Icarus/Assets/GameAssets/Cars/Scripts/Spawner.cs
+++ Project Icarus/Assets/GameAssets/Cars/Scripts/Spawner.cs
@@ -7,9 +7,9 @@ public class Spawner : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		spawnPoints[0] = new Vector3(-123,0,149.3);
-		spawnPoints[1] = new Vector3(-118.2,0,149.3);
-		spawnPoints[2] = new Vector3(-113.3,0,149.3);
+//		spawnPoints[0] = new Vector3(-123,0,149.3);
+//		spawnPoints[1] = new Vector3(-118.2,0,149.3);
+//		spawnPoints[2] = new Vector3(-113.3,0,149.3);
 	}
 	
 	// Update is called once per frame
