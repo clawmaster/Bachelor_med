550cef13cff03f303518562407035f882687d678
clawmaster <claes9@yahoo.dk>
2015-04-01 19:45:26 +0200

    Update of cars, spawner is putting cars in
    Cars are still drving wrongly.

--- Project Icarus/Assets/GameAssets/Cars/Scripts/Driving.cs
+++ Project Icarus/Assets/GameAssets/Cars/Scripts/Driving.cs
@@ -4,22 +4,78 @@ using System.Collections;
 public class Driving : MonoBehaviour {
 
 	public Transform mover; //the object being moved
-	private int speed = 0;
+	private int speed = 5;
 	public float SnapTo = 0.5f; //how close we get before snapping to the desination
 	private Vector3 destination = Vector3.zero; //where we want to move
 
 	// possible movement patterns.
 	private int groupCars;
-	private Vector3[] NextCheckpoint = new Vector3[3];
+	private int directionZ;
+	private int directionX;
+	private Vector3[] NextCheckpoint;
+	private Vector3[] driveStraight = new Vector3[1];
+	private Vector3[] driveLeft = new Vector3[5];
+	private Vector3[] driveRight = new Vector3[5];
 	private int currentCheckPoint = 0;
 
 
 	// Use this for initialization
 	void Start () {
-
+		// binds this car to the script
 		mover = this.transform;
+		if (mover.position.z > 0) 
+		{
+			directionZ = -1;
+		} else 
+		{
+			directionZ = 1;
+		}
+		if (mover.position.x > 0) 
+		{
+			directionX = -1;
+		} else 
+		{
+			directionX = 1;
+		}
+
+			//Driving straight ahead
+		driveStraight [0] = new Vector3 (0 * directionX + mover.position.x,0, 305 * directionZ + mover.position.z);
+		//Driving in a U, going left at incection 2
+		driveLeft [0] = new Vector3 (0 * directionX + mover.position.x, 0, 149 * directionZ + mover.position.z);
+		driveLeft [1] = new Vector3 (34 * directionX + mover.position.x,0,152 * directionZ + mover.position.z);
+		driveLeft [2] = new Vector3 (293 * directionX + mover.position.x,0,152 * directionZ + mover.position.z);
+		driveLeft [3] = new Vector3 (297 * directionX + mover.position.x,0,140 * directionZ + mover.position.z);
+		driveLeft [4] = new Vector3 (297 * directionX + mover.position.x,0,0 * directionZ + mover.position.z);
+		//Driving right at the 2nd intersection
+		driveRight [0] = new Vector3 (0 * directionX + mover.position.x,0,149 * directionZ + mover.position.z);
+		driveRight [1] = new Vector3 (34 * directionX + mover.position.x,0,152 * directionZ + mover.position.z);
+		driveRight [2] = new Vector3 (279 * directionX + mover.position.x,0,152 * directionZ + mover.position.z);
+		driveRight [3] = new Vector3 (281 * directionX + mover.position.x,0,163 * directionZ + mover.position.z);
+		driveRight [4] = new Vector3 (281 * directionX + mover.position.x,0,305 * directionZ + mover.position.z);
+
+
+
+		// adjusting for group cars (there is 3 roads systems, which is dedicated to current car cased on group)
+		groupCars = Random.Range (1, 4);
+		switch (groupCars) {
+		case 1:
+			NextCheckpoint = driveStraight;
+			break;
+		case 2:
+			NextCheckpoint = driveLeft;
+			break;
+		case 3:
+			NextCheckpoint = driveRight;
+			break;
+		default:
+			Debug.Log("There was problem and car didnt get a group");
+			Destroy (this.gameObject);
+			break;
+		}
+
+		Debug.Log(" groupcars: " + groupCars);
+		
 		destination = mover.position; //set the destination to the objects position when the script is run the first time
-		NextCheckpoint[0] = new Vector3(-105,0,-105); NextCheckpoint[1] = new Vector3(-105,0,0); NextCheckpoint[2] = new Vector3(0,0,0);
 
 		//providing it with the wanted way of going.
 		destination = NextCheckpoint[0];
@@ -30,8 +86,8 @@ public class Driving : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		//accelerate the car.
-		Debug.Log (destination.x + destination.z);
-		if (Mathf.Abs(mover.position.x + mover.position.z) - Mathf.Abs(destination.x + destination.z) > 25 && speed < 30) 
+		Debug.Log (Mathf.Abs(Vector3.Distance(destination,mover.position)));
+		if (Mathf.Abs(Vector3.Distance(destination,mover.position)) > 25.0 && speed < 30) 
 		{
 			speed = speed + 1;
 		} 
--- Project Icarus/Assets/GameAssets/Cars/Scripts/Spawner.cs
+++ Project Icarus/Assets/GameAssets/Cars/Scripts/Spawner.cs
@@ -3,17 +3,34 @@ using System.Collections;
 
 public class Spawner : MonoBehaviour {
 
-	private Vector3[] spawnPoints = new Vector3[12];
+	public Transform car;
+	private Vector3[] spawnPoints = new Vector3[6];
+	private float timer;
+	private int randomSpawn;
+
 
 	// Use this for initialization
 	void Start () {
-//		spawnPoints[0] = new Vector3(-123,0,149.3);
-//		spawnPoints[1] = new Vector3(-118.2,0,149.3);
-//		spawnPoints[2] = new Vector3(-113.3,0,149.3);
+		spawnPoints [0] = new Vector3 (-123, 0, 149);
+		spawnPoints [1] = new Vector3 (-118, 0, 154);
+		spawnPoints [2] = new Vector3 (-113, 0, 159);
+		spawnPoints [3] = new Vector3 (-98, 0, -145);
+		spawnPoints [4] = new Vector3 (-103, 0,-150);
+		spawnPoints [5] = new Vector3 (-107, 0,-155);
+
+		timer = Time.time+5;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (timer < Time.time) 
+		{
+			randomSpawn = Random.Range (0, 6);
+			Instantiate(car,spawnPoints[randomSpawn],Quaternion.identity);
+			timer = Time.time + 5;
+		}
+
 	
 	}
 }
