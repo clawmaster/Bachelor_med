bc5c97414bb97e822b0979608a2e5e9544c930ac
Mathias8888 <Mathias8888@hotmail.com>
2015-04-25 00:17:38 +0200

    Added GameController

---
+++ Project Icarus/Assets/Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public float waitingTime = 15f;
+
+	private float currentTime;
+
+	// Use this for initialization
+	void Start () {
+
+		waitingTime = Time.time + waitingTime;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		currentTime = Time.time;
+
+		if (currentTime > waitingTime){
+			Application.LoadLevel("LoFi Scene");
+		}
+	
+	}
+}
