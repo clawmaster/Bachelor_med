10ab322576332d359fb528821e8065427cebea80
Claes Nielsen <claes9@yahoo.dk>
2015-04-27 01:51:19 +0200

    lowfi now with pedestrians
    Cylinder as pedistrians, will also be used on hifi most likely.

---
+++ Project Icarus/Assets/GameAssets/Streets/Pedestrian/Movement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public Vector3 goal;
+	NavMeshAgent agent;
+	private Vector3 lastPos = new Vector3(0,0,0);
+
+	// Use this for initialization
+	void Start () {
+		NavMeshAgent agent = GetComponent<NavMeshAgent>();
+		if (transform.position.z > 0)
+		{
+			if(transform.position.x > 185 || transform.position.x < -125) 
+				goal = transform.position + new Vector3(0,0,-200f);
+			else if (transform.position.x > 150 && transform.position.x < 185)
+				goal = transform.position + new Vector3(-248,0,0);
+			else 
+				goal = transform.position + new Vector3(248,0,0);
+		}
+		else
+		{
+			if(transform.position.x > 185 || transform.position.x < -125) 
+				goal = transform.position + new Vector3(0,0,200f);
+			else if (transform.position.x > 150 && transform.position.x < 185)
+				goal = transform.position + new Vector3(-248,0,0);
+			else 
+				goal = transform.position + new Vector3(248,0,0);
+		}
+
+
+
+		agent.destination = goal;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(lastPos == transform.position)
+		{
+			Destroy(this.gameObject);
+		}
+
+		lastPos = transform.position;
+
+	
+	}
+}
